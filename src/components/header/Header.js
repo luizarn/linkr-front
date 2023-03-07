a4422d9ac6d55901c39780e4c7f5b013a8a254fb
gavuolo <gabs.vuolo@gmail.com>
2023-03-07 18:42:46 -0400

    Feat: componentização header

---
+++ src/components/header/Header.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+export default function Header() {
+    return (
+      <>
+        <HeaderBox>
+            
+        </HeaderBox>
+      </>
+    );
+  }
+  const HeaderBox = styled.div`
+    width: 100%;
+    height: 75px;
+    position: fixed;
+    background-color: #151515;
+  h1 {
+    font-family: 'Passion One';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 49px;
+    line-height: 54px;
+    color: white;
+  }
+  `
