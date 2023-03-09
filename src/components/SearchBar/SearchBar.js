b390e149084fb0281402ec64dde23eee81c8e40d
Davi <daviribeirotesta@gmail.com>
2023-03-08 21:01:06 -0300

    feat/debounce functions

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,14 @@
 import { DebounceInput } from "react-debounce-input";
 import { HiSearch } from "react-icons/hi";
 import { SearchContainer, HiddenBox, HiddenUsers } from "./style";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function SearchBar() {
 
     const [value, setValue] = useState();
     const [open, setOpen] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [debouncedValue, setDebouncedValue] = useState('');
 
     const user = [
         {
@@ -21,7 +23,29 @@ export default function SearchBar() {
             username: "Josias",
             url: "https://ps.w.org/user-avatar-reloaded/assets/icon-128x128.png?rev=2540745"
         }
-    ]
+    ];
+
+    // useEffect(() => {
+    //     const timerId = setTimeout(() => {
+    //       setDebouncedValue(value);
+    //     }, 300);
+
+    //     return () => {
+    //         clearTimeout(timerId);
+    //       };
+    //     }, [value]);    
+
+    //     useEffect(() => {
+    //         async function searchUsers() {
+    //           if (debouncedValue.length >= 3) {
+    //             const { data } = await axios.get(`/users?search=${debouncedValue}`);
+    //             setUsers(data);
+    //           } else {
+    //             setUsers([]);
+    //           }
+    //         }
+    //         searchUsers();
+    //     }, [debouncedValue]); 
 
     const handleSearchIconClick = () => {
         setOpen((prevState) => !prevState);
@@ -36,6 +60,7 @@ export default function SearchBar() {
                 placeholder="Search for people"
                 onChange={(e) => setValue(e.target.value)}
                 debounceTimeout={300}
+                minLength={3}
             />
             <HiSearch
                 className="icon"
@@ -43,6 +68,12 @@ export default function SearchBar() {
             />
 
             <HiddenBox open={open}>
+                {/* {users.map(({pictureUrl, username}) => (
+                    <HiddenUsers>
+                        <img src={pictureUrl} alt="" />
+                        <h2>{username}</h2>
+                    </HiddenUsers>
+                ))} */}
                 {user.map(({ url, username }) => (
                     <HiddenUsers>
                         <img src={url} alt="" />
